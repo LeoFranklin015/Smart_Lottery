b5e2274b084724eca23d1224d4478d0a8b34ad67
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-27 17:13:58 +0530

    Test updates

--- test/unit/Lottery.test.js
+++ test/unit/Lottery.test.js
@@ -41,39 +41,97 @@
 //       });
 //     });
 
-
-const { assert, expect } = require("chai")
-const { network, deployments, ethers } = require("hardhat")
-const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+const { assert, expect } = require("chai");
+const { network, deployments, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../../helper-hardhat-config");
 
 !developmentChains.includes(network.name)
-    ? describe.skip
-    : describe("Raffle Unit Tests", function () {
-          let raffle, raffleContract, vrfCoordinatorV2Mock, raffleEntranceFee, interval, player // , deployer
+  ? describe.skip
+  : describe("Raffle Unit Tests", function () {
+      let raffle,
+        raffleContract,
+        vrfCoordinatorV2Mock,
+        raffleEntranceFee,
+        interval,
+        player; // , deployer
 
-          beforeEach(async () => {
-              accounts = await ethers.getSigners() // could also do with getNamedAccounts
-              //   deployer = accounts[0]
-              player = accounts[1]
-              await deployments.fixture(["mocks", "raffle"]) // Deploys modules with the tags "mocks" and "raffle"
-              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock") // Returns a new connection to the VRFCoordinatorV2Mock contract
-              raffleContract = await ethers.getContract("Raffle") // Returns a new connection to the Raffle contract
-              raffle = raffleContract.connect(player) // Returns a new instance of the Raffle contract connected to player
-              raffleEntranceFee = await raffle.getEntranceFee()
-              interval = await raffle.getInterval()
-          })
-                    describe("constructor", function () {
-              it("initializes the raffle correctly", async () => {
-                  // Ideally, we'd separate these out so that only 1 assert per "it" block
-                  // And ideally, we'd make this check everything
-                  const raffleState = (await raffle.getRaffleState()).toString()
-                  // Comparisons for Raffle initialization:
-                  assert.equal(raffleState, "0")
-                  assert.equal(
-                      interval.toString(),
-                      networkConfig[network.config.chainId]["keepersUpdateInterval"]
-                  )
-              })
-          })
+      beforeEach(async () => {
+        accounts = await ethers.getSigners(); // could also do with getNamedAccounts
+        //   deployer = accounts[0]
+        player = accounts[1];
+        await deployments.fixture(["mocks", "raffle"]); // Deploys modules with the tags "mocks" and "raffle"
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock"); // Returns a new connection to the VRFCoordinatorV2Mock contract
+        raffleContract = await ethers.getContract("Raffle"); // Returns a new connection to the Raffle contract
+        raffle = raffleContract.connect(player); // Returns a new instance of the Raffle contract connected to player
+        raffleEntranceFee = await raffle.getEntranceFee();
+        interval = await raffle.getInterval();
+      });
+      describe("constructor", function () {
+        it("initializes the raffle correctly", async () => {
+          // Ideally, we'd separate these out so that only 1 assert per "it" block
+          // And ideally, we'd make this check everything
+          const raffleState = (await raffle.getRaffleState()).toString();
+          // Comparisons for Raffle initialization:
+          assert.equal(raffleState, "0");
+          assert.equal(
+            interval.toString(),
+            networkConfig[network.config.chainId]["keepersUpdateInterval"]
+          );
+        });
+      });
+      describe("Enter Raffle", async () => {
+        it("reverts when you dont pay enough", async () => {
+          await expect(raffle.enterRaffle()).to.be.revertedWith(
+            "Raffle__SendMoreToEnterRaffle"
+          );
+        });
+        it("It stores player when a he enters", async () => {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          const playerJoinedNow = await raffle.getPlayer(0);
+          assert.equal(playerJoinedNow, player.address);
+        });
+        it("emits an event", async () => {
+          await expect(
+            raffle.enterRaffle({ value: raffleEntranceFee })
+          ).to.emit(raffle, "RaffleEnter");
+        });
+        it("doesnt allow when raffle is closed", async () => {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.send("evm_mine", []);
 
-        })
+          await raffle.performUpkeep([]);
+          await expect(
+            raffle.enterRaffle({ value: raffleEntranceFee })
+          ).to.be.revertedWith("Raffle__RaffleNotOpen");
+        });
+      });
+      describe("CheckUpkeep", async () => {
+        it("Doesnt run when no parameters are passed", async () => {
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.send("evm_mine", []);
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([]);
+          assert(!upkeepNeeded);
+        });
+        it("Doesnt run when the raffle is closed", async () => {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          await network.provider.send("evm_increaseTime", [
+            interval.toNumber() + 1,
+          ]);
+          await network.provider.send("evm_mine", []);
+          await raffle.performUpkeep([]);
+          const raffleState = await raffle.getRaffleState();
+          const { upkeepNeeded } = await raffle.callStatic.checkUpkeep([]);
+          assert.equal(raffleState.toString(), "1");
+          assert.equal(upkeepNeeded, false);
+        });
+        it("");
+      });
+    });
