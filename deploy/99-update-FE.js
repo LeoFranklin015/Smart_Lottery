3711b762e1404ef594bf511e6511ee83e1e42338
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2023-07-31 16:24:12 +0530

    automated updateting the abi and address for fe

--- deploy/99-update-FE.js
+++ deploy/99-update-FE.js
@@ -1,3 +1,8 @@
+const { ethers, network } = require("hardhat");
+const fs = require("fs");
+const FRONT_END_ABI_ADDRESS = "../fe-lottery/src/constants/abi.json";
+const FRONT_END_ADDRESSES_FILE =
+  "../fe-lottery/src/constants/contractAddress.json";
 module.exports = async () => {
   if (process.env.UPDATE_FE) {
     console.log("Update FE");
@@ -5,3 +10,29 @@ module.exports = async () => {
     updateABI();
   }
 };
+
+const updateABI = async () => {
+  const raffle = await ethers.getContract("Raffle");
+  fs.writeFileSync(
+    FRONT_END_ABI_ADDRESS,
+    raffle.interface.format(ethers.utils.FormatTypes.json)
+  );
+};
+
+const updateContractAddress = async () => {
+  const raffle = await ethers.getContract("Raffle");
+  const chainId = network.config.chainId.toString();
+  const currentAddress = JSON.parse(
+    fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8")
+  );
+  if (chainId in currentAddress) {
+    if (!currentAddress[chainId].includes(raffle.address)) {
+      currentAddress[chainId].push(raffle.address);
+    }
+  } else {
+    currentAddress[chainId] = [raffle.address];
+  }
+  fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddress));
+};
+
+module.exports.tags = ["all", "frontend "];
